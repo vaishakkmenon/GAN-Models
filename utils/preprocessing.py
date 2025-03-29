52e8115db8b6aa63f65c3f18b607cedfa597687f
Vaishak Menon <vaishakkmenon@gmail.com>
2025-03-29 17:00:24 +0000

    updated the wrong function

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -23,35 +23,7 @@ class CelebADataset(Dataset):
 
         return image
 
-def load_celeba(provided_path="img_align_celeba/", batch_size=64, num_workers=8):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
-
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.abspath(os.path.join(current_dir, ".."))
-    data_path = os.path.join(project_root, "data", provided_path)
-
-    print(f"[INFO] Resolved dataset path: {data_path}")
-    
-    if project_root not in sys.path:
-        sys.path.append(project_root)
-
-    if not os.path.exists(data_path):
-        raise FileNotFoundError(f"The resolved path '{data_path}' does not exist.")
-
-    train_dataset = datasets.MNIST(data_path, train=True, transform=transform)
-    test_dataset = datasets.MNIST(data_path, train=False, transform=transform)
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    print("[INFO] Training Data Loaded")
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    print("[INFO] Testing Data Loaded")
-    
-    return train_loader, test_loader
-
-def load_mnist(provided_path="mnist/", batch_size=64, num_workers=8):
+def load_data(provided_path="img_align_celeba/", batch_size=64, num_workers=8):
     transform = transforms.Compose([
         transforms.CenterCrop(178),
         transforms.Resize(128),
@@ -71,8 +43,36 @@ def load_mnist(provided_path="mnist/", batch_size=64, num_workers=8):
     if not os.path.exists(data_path):
         raise FileNotFoundError(f"The resolved path '{data_path}' does not exist.")
 
-    train_dataset = datasets.MNIST(data_path, train=True, transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    train_data = CelebADataset(data_path, transform=transform)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     print("[INFO] Training Data Loaded")
     
     return train_loader
+
+def load_mnist(provided_path="mnist/", batch_size=64, num_workers=8):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.abspath(os.path.join(current_dir, ".."))
+    data_path = os.path.join(project_root, "data", provided_path)
+
+    print(f"[INFO] Resolved dataset path: {data_path}")
+    
+    if project_root not in sys.path:
+        sys.path.append(project_root)
+
+    if not os.path.exists(data_path):
+        raise FileNotFoundError(f"The resolved path '{data_path}' does not exist.")
+
+    train_dataset = datasets.MNIST(data_path, train=True, transform=transform, download=True)
+    test_dataset = datasets.MNIST(data_path, train=False, transform=transform, download=True)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    print("[INFO] Training Data Loaded")
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    print("[INFO] Testing Data Loaded")
+    
+    return train_loader, test_loader
